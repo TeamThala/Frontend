f6ce0217bb86a1263469e71e8f71a15ae0e7516c
AnshSaluja <anujsureshbhai.dakhara@stonybrook.edu>
2025-04-14 21:35:13 -0400

    merged max's branch to anuj

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }], // Transpile modern JS for the current Node.js version
-      '@babel/preset-typescript', // Add TypeScript support
-    ],
-  };
